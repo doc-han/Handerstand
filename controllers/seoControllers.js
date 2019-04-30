66fbb37a270de84a701a747bc1515490b63dcaf6
Han <yahyafarhan48@gmail.com>
2019-04-30 16:20:01 +0000

    sitemap redefined

--- controllers/seoControllers.js
+++ controllers/seoControllers.js
@@ -11,21 +11,21 @@ module.exports = function(app){
     app.get('/sitemap.xml',(req,res)=>{
         res.setHeader('content-type', 'application/xml');
         var xml = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">';
+        xml += `<url><loc>${site}</loc><changefreq>daily</changefreq><priority>1.0</priority></url>`;
         post.find({active:true}).select("url date")
         .then(doc=>{
             doc.forEach(i=>{
                 var url=`${site}/${i.url}`;
                 xml += `<url><loc>${url}</loc>`;
-                xml += `<lastmod>${i.date.toISOString()}</lastmod></url>`;
+                xml += `<lastmod>${i.date.toISOString()}</lastmod><priority>0.7</priority></url>`;
             })
 
             tag.find().select("code")
             .then(tg=>{
-                console.log(tg);
                 
                 tg.forEach(i=>{
                     var url=`${site}/${i.code}`;
-                    xml += `<url><loc>${url}</loc></url>`;
+                    xml += `<url><loc>${url}</loc><changefreq>daily</changefreq><priority>0.8</priority></url>`;
                 })
                 xml += `</urlset>`;
                 res.send(xml);
